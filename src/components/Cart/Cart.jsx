780ba4e1010eb093edc43edbe706f42afde72681
Shourovcool <amithasan319@gmail.com>
2023-10-24 21:37:36 +0600

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,15 @@ const Cart = ({ cart }) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        totalPrice = totalPrice + product.price;
+        // if (product.quantity === 0) {
+        //     product.quantity = 1;
+        // }
+
+        totalPrice = totalPrice + product.price * quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice * 7 / 100;
@@ -19,7 +25,7 @@ const Cart = ({ cart }) => {
     return (
         <div className='cart'>
             <h4>Delivery Spider: <big>Aronno</big></h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Shipping Cost: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
